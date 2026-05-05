c04954ab6eca463d7a3e48f984fd04ab1e8a8349
Lilly Vitória <lv212474@gmail.com>
2026-05-05 11:25:42 -0300

    Aula 5.5.26

--- Lilly/AULA 7/sétimo.py
+++ Lilly/AULA 7/sétimo.py
@@ -1,1 +1,96 @@
 # Projeto Cancela Automática
+
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+#
+# Passo 1:  
+# Perguntar informações sobre o veiculo ou forma acesso
+# Pressionar o botao para emitir ticket
+# Verificar se possui TAG para acesso liberado
+# Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+
+# print("Bem-vindo ao Shopping Lilly's")
+# informaçõesV = float(input("Digite seu modelo de carro e placa"))
+# print(f"Seu veiculo é:" ,informaçõesV)
+
+# escolha = input("Digite sua forma de acesso")
+# print(f"Sua forma de acesso será:" ,escolha)
+# for FormaAcesso in escolha:
+#       if escolha == "ticket":
+#           print("Aperte o botão para emitir o ticket")
+#       elif escolha == "tag":
+#           print("Acesso liberado!")
+#       elif escolha == "Interfone":
+#           print("Você está sendo direcionado para o interfone!")
+# else:
+#     print("Error de usuário!")
+# hora_entrada = float(input("Digite sua hora de entrada"))
+# ("_____________________//__________________________")
+
+# TempoP = float(input("Digite a hora de saída"))
+# ValorC = 2 
+# total_permanencia = TempoP - hora_entrada
+# total_estacionamento = total_permanencia * ValorC
+# print(f"O valor foi de R${total_estacionamento:.2f}"")
+# print("Devolver Ticket")
+# elif metodo_entrada == "Tag":
+#    print("Bem-Vindo ao Shopping")
+#    print("Sua permanência no Shopping será cobrada na sua fatura")
+# elif metodo_entrada == "Interfone":
+#     print("Bem-Vindo ao Shopping Lilly's")
+#     print("Liberando acesso pelo Interfone")
+#     print("Sua saída deverá ser feita também pelo Interfone")
+
+# else:
+#     print("Obrigado pela visita!")
+
+
+print("Bem-vindo ao Shopping Lilly's")
+("___________________*__________________*_________________")
+print("Escolha as opções")
+print("1 - Ticket \n 2 - TAG \n 3 - Interfone \n")
+metodo_entrada = input("Ticket / Tag / Interfone \n")
+
+if metodo_entrada == "Ticket":
+    print("Bem-Vindo ao Shopping Lilly's")
+    hora_entrada = float(input("Digite a hora de entrada"))
+    valor_estacionamento = float(input("Digite o valor a cobrar"))
+    hora_saida = float(input("Digite o hora de saída"))
+    total_permanencia = hora_saida - hora_entrada
+    print(f"Seu tempo de permanência {total_permanencia} em horas")
+    total_estacionamento = total_permanencia * valor_estacionamento
+    print(f"O valor a ser cobrado foi de R${total_estacionamento:.2f}")
+    print("Devolver Ticket")    
+
+    
+elif metodo_entrada == "Tag":
+    print("Bem-Vindo ao Shopping")
+    print("Sua permanência no Shopping será cobrada na sua fatura")
+
+elif metodo_entrada == "Interfone":
+    print("Bem-Vindo ao Shopping Lilly's")
+    print("Liberando acesso pelo Interfone")
+    print("Sua saída deverá ser feita também pelo Interfone")
+
+else:
+    print("Obrigado pela visita!")
+("____________________*__________________*_________________")
---
+++ Lilly/AULA 8/oito.py
@@ -0,0 +1,147 @@
+# Situação de Aprendizagem – Atividade Individual
+# Exercícios de Programação Python: "O Caça-Erros"
+
+# 1. O Problema da Idade
+
+# idade = input("Digite sua idade:")
+# if idade >=18:
+#     print("Você é maior de idade.")
+
+# 2. A Escrita Fiel
+
+# nome = "Mariana"
+# print("Seja Bem-vinda,nome!")
+
+# 3. Falta de Espaço
+
+# numero = 10
+# if numero >5:
+#     print("O numero é maior que cinco.")
+# else:
+#     print("O número é o menor ou igual a cinco.")
+
+# 4. Esquecimento Fatal
+
+# usuario = "aluno123"
+# if usuario == "alun123"
+# print("Login realizado com sucesso.")
+
+# 5. Atribuição vs. Comparação
+
+# clima = "ensolarado"
+# if clima = "chuvoso":
+#  print("Login realizado com sucesso!")
+
+# 6. Misturando Alhos com Bugalhos
+
+# pontos = 50
+# print("Parabéns! Você fez " + pontos +"pontos.")
+
+
+# 7. A Ordem dos Fatores
+# nota = 9.5
+# if nota >=7:
+#  print("Aprovado!")
+# elif nota >=9:
+#  print("Excelente!")
+
+
+# 8. O Contador de 1 a 5
+# for i in range  (5):
+#   print("i")
+
+# 9. O Loop Eterno
+
+# tentativas = 1
+# while tentativas <= 3:
+#  print("Tentando conectar...")
+ 
+
+# 10. A Senha Teimosa
+
+# senha = ""
+# while senha == "python123":
+#  senha = input("Digite a senha secreta:")
+# print("Acesso concedido!")
+
+
+
+
+
+# CORRIGIDO e MELHORIAS:
+
+# idade =  int(input("Digite sua idade:"))
+# if idade == [18]:
+#     print("Você é maior de idade.")
+# else:
+#      print("Você é menor de idade.")
+
+
+("*************************************")
+
+# nome = "Mariana"
+# print("Seja Bem-vinda!", nome)
+
+
+("*************************************")
+
+# numero = 10
+# if numero > 5:
+#      print("O numero é maior que cinco.")
+
+# else:
+#      print("O número é o menor ou igual a cinco.") 
+    
+
+("*************************************")
+
+# usuario = "aluno123"
+# if usuario == "aluno123":  
+#     print("Login realizado com sucesso.")
+# else:
+#     print("ERROR no login!")
+
+
+("************************************")
+
+
+# clima = input("qual o clima de hoje?")
+# if clima == "chuvoso":
+#  print("Leve um guarda-chuva!")
+# elif clima == "ensolarado":
+#  print("Use óculos de sol")
+
+("************************************")
+
+# pontos = 50
+# print("Parabéns! Você fez ", + pontos,  + pontos)
+
+
+("***********************************")
+
+# nota = int(input("qual foi sua nota?"))
+# if nota ==7:
+#     print("Aprovado!")
+# elif nota <=9:
+#   print("Excelente!")
+# else:
+#   int(print("Que horror!"))
+
+("***********************************")
+
+# i = ["1, 2, 3, 4, 5"]
+# for i in range  (5):
+#    print("Mostrar na tela os números", i)
+
+
+("***********************************")
+
+# tentativas = 1
+# while tentativas <= 3:
+#  print("Tentando conectar...")
+
+
+("****************************************")
+
+# senha = input("Digite a senha secreta:")
+# print("Acesso concedido!")
