09ef2c711efcddd9055093f35bffcc5c812715e2
Lilly Vitória <lv212474@gmail.com>
2026-05-19 11:04:41 -0300

    atualizações

--- Lilly/LOPAL/AULA 9/nove.py
+++ Lilly/LOPAL/AULA 9/nove.py
@@ -1,23 +1,23 @@
-print("Bem-Vinda Lírioxente")
-print("Nossa robo ira responder aos comandos dados!")
-print("Lírioxente, acorde!")
+# print("Bem-Vinda Lírioxente")
+# print("Nossa robo ira responder aos comandos dados!")
+# print("Lírioxente, acorde!")
 
 
-peça = input("Lírioxente, qual peça deseja pegar?")
-if peça == "Vermelho":
- print("Lírioxente, vire 175° e ande 50 passos para frente")
-print(f"pegue a peça {peça} e volte para a base")
-print("Entregue a peça!")
+# peça = input("Lírioxente, qual peça deseja pegar?")
+# if peça == "Vermelho":
+#  print("Lírioxente, vire 175° e ande 50 passos para frente")
+# print(f"pegue a peça {peça} e volte para a base")
+# print("Entregue a peça!")
 
-elif peça == "Verde":
-print("Vire 175° e ande 20 passos para frente")
-print(f"pegue a peça {peça} e volte para a base")
-print("Entregue a peça!")
+# elif peça == "Verde":
+# print("Vire 175° e ande 20 passos para frente")
+# print(f"pegue a peça {peça} e volte para a base")
+# print("Entregue a peça!")
 
-else:
- print("Error!")
+# else:
+# print("Error!")
 
- reniciação = input("Robo deseja continuar o circuito?")
+# reniciação = input("Robo deseja continuar o circuito?")
 
 
 # if reniciação == "Sim":
