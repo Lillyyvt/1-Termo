5574e2facab6b53ac1876b76cd9542cfa2c7f603
Lilly Vitória <lv212474@gmail.com>
2026-05-19 11:01:42 -0300

    Atualização da AULA 11

---
+++ Lilly/LOPAL/AULA 10 ; Projeto/dez.py
@@ -0,0 +1,75 @@
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+#
+# Passo 1:  
+# Perguntar informações sobre o veiculo ou forma acesso
+# Pressionar o botao para emitir ticket
+# Verificar se possui TAG para acesso liberado
+# Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+
+while True:
+ print("Bem-vindo ao estacionamento do Shopping lírioxente")
+  
+# 　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ
+
+ informações = input("Qual sua marca e placa do carro")
+ Relatório = print("Relatório \n Opção 1: TAG \n Opção 2: Ticket \n Opção 3: Interfone")
+ acesso = input("Como deseja seu acesso?")
+ if acesso == "TAG":
+    print("Acesso liberado! Aproveite o passeio")
+ elif acesso == "Ticket":
+    print("Aperte o botão para emitir o ticket! \n Aproveite o passeio")
+ elif acesso == "Intefone":
+    print("Estamos lhe encaminhando para a ligação!")
+ else:
+    print("Error! Tente novamente.")
+ break
+# 　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ
+
+tempo = float(input("Qual foi seu tempo de permanência?"))
+preço = 4 # $ por hora
+total = tempo * preço
+
+for saída in acesso:
+    if acesso == "Ticket":
+        print(f"Você deve pagar ${total}")
+    elif acesso == "TAG":
+        print(f"Geraremos a fatura de ${total} no TAG")
+    else:
+        print("Error, tente novamente ou acesse o interfone!")
+    break
+ 
+# 　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ
+
+saída = input("Deseja o relatório do carro?")      
+for relatório in saída: 
+    if saída == "sim":
+        print(f" Carro: {informações} | Acesso: {acesso} | Permanência: {tempo} | Total: {total} ")
+        break
+    elif saída == "não":
+        print("Agradecemos a preferencia!")
+    else:
+     print("Obrigada por escolher nosso Shopping lírioxente. Tenha uma Boa tarde!")
+
+print("Finalizando Programa \n Obrigada por escolher nosso Shopping lírioxente. Tenha uma Boa tarde!")
+
+# 　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ　ﾟ･｡･ﾟ
